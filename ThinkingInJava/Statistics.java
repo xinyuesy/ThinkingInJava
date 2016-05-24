1b1e78b8a218d6af655608ae27bfcea25d36335c
xinyuesy <xinyuesy@qq.com>
2016-05-24 16:05:42 +0800

    compile pass

--- ThinkingInJava/Statistics.java
+++ ThinkingInJava/Statistics.java
@@ -1,15 +1,23 @@
+package javatest;
+
 import java.util.*;
 
-class Stattistics
+class Statistics
 {
 	public static void main(String[] args)
 	{
-		Hashtable ht = new Hashtable();
+		Hashtable<Integer,Integer> ht = new Hashtable<Integer,Integer>();
 		for(int i = 0; i < 10000; i++)
 		{
 			Integer r = new Integer((int)(Math.random()*20));
+			Integer temp;
 			if(ht.containsKey(r))
-				ht.get(r)++;
+			{
+				temp = ht.get(r);
+				temp++;
+				ht.put(r,temp);
+			}
+
 			else
 				ht.put(r,new Integer(1));
 		}
