8538c936d6af17ba46672d775e370dc0abf17e85
xinyuesy <xinyuesy@qq.com>
2016-05-27 21:29:40 +0800

    Update2016052703

--- JavaTest/src/access/Connection.java
+++ JavaTest/src/access/Connection.java
@@ -2,7 +2,20 @@ package access;
 
 public class Connection {
 	//private static Connection c = new Connection();
+	
+	private static int value1 = 1;
+	private int value2 = 1;
 	Connection(){}
+	
+	public int getValue1()
+	{
+		return value1;
+	}
+	
+	public int getValue2()
+	{
+		return value2;
+	}
 	
 	
 	
